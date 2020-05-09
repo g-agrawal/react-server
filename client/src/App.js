cc380af3b6b6273f07fb2ab901a968c550c01fe9
g-agrawal <er.gourav.agrawal@gmail.com>
2020-05-09 20:07:29 +0530

    removed react-bootstrap module and added bootstrap as file in index.html

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BootstrapNavbar from './componenets/BootstrapNavbar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './componenets/Home';
+import About from './componenets/About';
+import Contact from './componenets/Contact';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <BootstrapNavbar />
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <BootstrapNavbar />
+        <Switch>
+          <Route exact path ='/' component={Home} />
+          <Route exact path ='/About' component={About} />
+          <Route exact path ='/Contact' component={Contact} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+   );
+  }
 }
 
 export default App;
--- client/src/componenets/BootstrapNavbar.js
+++ client/src/componenets/BootstrapNavbar.js
@@ -1,47 +1,29 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route, Link  } from "react-router-dom";
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
-import Home from './Home'
-import About from './About';
-import Contact from './Contact';
+import { Link  } from "react-router-dom";
 
-class BootstrapNavbar extends React.Component{
+class BootstrapNavbar extends React.Component {
     render(){
         return(
-            <div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <BrowserRouter>
-                            <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                                <Navbar.Brand>
-                                    <Nav className="mr-auto">
-                                        <Nav.Link as={Link} to="/">Open Sky</Nav.Link>
-                                    </Nav>
-                                </Navbar.Brand>
-                                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                                <Navbar.Collapse id="responsive-navbar-nav">
-                                    <Nav className="mr-auto">
-                                        <Nav.Link as={Link} to="/" >Home</Nav.Link>
-                                        <Nav.Link as={Link} to="/About" >About</Nav.Link>
-                                        <Nav.Link as={Link} to="/Contact" >Contact</Nav.Link>
-                                    </Nav>
-                                    <Form inline>
-                                        <FormControl type="text" placeholder="Search" className="mr-sm-2" size="sm"/>
-                                        <Button variant="outline-success">Search</Button>
-                                    </Form>
-                                </Navbar.Collapse>
-                            </Navbar>
-                            <br />
-                            <Switch>
-                                <Route exact path="/" component={Home} />
-                                <Route path="/About" component={About} />
-                                <Route path="/Contact" component={Contact} />
-                            </Switch>
-                        </BrowserRouter>
-                    </div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">Open Sky</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>                
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">Home</Link>
+                        </li> 
+                        <li className="nav-item">
+                            <Link to="/About" className="nav-link">About</Link>
+                        </li>  
+                        <li className="nav-item">
+                            <Link to="/Contact" className="nav-link">Contact</Link>
+                        </li>                     
+                    </ul>
                 </div>
-            </div>
-        )  
+            </nav>
+        );     
     }
 }
 
